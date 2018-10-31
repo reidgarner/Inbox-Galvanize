6062e9872fccdd0d179a6841528bee73ccb58b7c
reidgarner <reidbgarner@gmail.com>
2018-10-31 11:34:39 -0600

    Moved first message to Message component and made it a child of MessageList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import MessageList from './components/MessageList'
-import Message from './components/Message'
 import Toolbar from './components/Toolbar'
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <div className="App">
         <Toolbar />
         <MessageList />
-        <Message />
       </div>
     );
   }
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import '../App.css';
+import Message from './Message'
 
 const MessageList = (props) => {
     return (
-        <div class="row message unread">
-            <div class="col-xs-1">
-                <div class="row">
-                    <div class="col-xs-2">
-                        <input type="checkbox" />
-                    </div>
-                    <div class="col-xs-2">
-                        <i class="star fa fa-star-o"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-11">
-                <a href="#">
-                    Here is some message text that has a bunch of stuff
-                </a>
-            </div>
-        </div>
+        <Message />
     )
 }
 
